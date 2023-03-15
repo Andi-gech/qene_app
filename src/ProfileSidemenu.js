0a5f8fb181975b76f1736164af2a91fa5c31652d
Andi-gech <andi.fab23@gmail.com>
2023-03-15 16:23:40 +0300

    myapp

--- src/ProfileSidemenu.js
+++ src/ProfileSidemenu.js
@@ -24,85 +24,85 @@ function ProfileSidemenu() {
   if (location.pathname === "/login") {
     return null;
   }
-  if (data && user) {
-    return (
-      <div className="Sidemenu">
-        <div className="ProfileSidemenu">
-          <img
-            src={
-              data.profile_pic?.startsWith("http")
-                ? data.profile_pic
-                : `https://andigech.pythonanywhere.com${data.profile_pic}`
-            }
-            alt="logo"
-          />
-          <h2>{user.username}</h2>
-          {user?.is_staff && (
-            <p>
-              Admin <FaWrench />
-            </p>
-          )}
-        </div>
-        <div className="Buttons-menu">
-          <div className="menus">
-            <Link to={"courses"}>
-              <div className="Menubutton">
-                <span id="menuicon">
-                  <FaList />
-                </span>
 
-                <p>All Course</p>
-              </div>
-            </Link>
-            <Link to="/mycourse">
-              <div className="Menubutton">
-                <span id="menuicon">
-                  <FaBook />
-                </span>
+  return (
+    <div className="Sidemenu">
+      <div className="ProfileSidemenu">
+        <img
+          src={
+            data?.profile_pic?.startsWith("http")
+              ? data.profile_pic
+              : `https://andigech.pythonanywhere.com${data.profile_pic}`
+          }
+          alt="logo"
+        />
+        <h2>{user?.username}</h2>
+        {user?.is_staff && (
+          <p>
+            Admin <FaWrench />
+          </p>
+        )}
+      </div>
+      <div className="Buttons-menu">
+        <div className="menus">
+          <Link to={"courses"}>
+            <div className="Menubutton">
+              <span id="menuicon">
+                <FaList />
+              </span>
 
-                <p>My Course</p>
-              </div>
-            </Link>
-            <Link to={"grade"}>
-              <div className="Menubutton">
-                <span id="menuicon">
-                  <FaPercent />
-                </span>
+              <p>All Course</p>
+            </div>
+          </Link>
+          <Link to="/mycourse">
+            <div className="Menubutton">
+              <span id="menuicon">
+                <FaBook />
+              </span>
 
-                <p>Grade</p>
-              </div>
-            </Link>
-            <Link>
-              <div className="Menubutton">
-                <span id="menuicon">
-                  <FaProjectDiagram />
-                </span>
+              <p>My Course</p>
+            </div>
+          </Link>
+          <Link to={"grade"}>
+            <div className="Menubutton">
+              <span id="menuicon">
+                <FaPercent />
+              </span>
 
-                <p>Projects</p>
-              </div>
-            </Link>
-            <Link>
-              <div className="Menubutton">
-                <span id="menuicon">
-                  <FaDiscord />
-                </span>
+              <p>Grade</p>
+            </div>
+          </Link>
+          <Link>
+            <div className="Menubutton">
+              <span id="menuicon">
+                <FaProjectDiagram />
+              </span>
 
-                <p>Discusion</p>
-              </div>
-            </Link>
-            <Link onClick={() => signOut()}>
-              <div className="Menubutton">
-                <span id="menuicon">
-                  <FaSignOutAlt />
-                </span>
+              <p>Projects</p>
+            </div>
+          </Link>
+          <Link>
+            <div className="Menubutton">
+              <span id="menuicon">
+                <FaDiscord />
+              </span>
 
-                <p>Logout</p>
-              </div>
-            </Link>
-          </div>
+              <p>Discusion</p>
+            </div>
+          </Link>
+          <Link onClick={() => signOut()}>
+            <div className="Menubutton">
+              <span id="menuicon">
+                <FaSignOutAlt />
+              </span>
+
+              <p>Logout</p>
+            </div>
+          </Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default ProfileSidemenu;
